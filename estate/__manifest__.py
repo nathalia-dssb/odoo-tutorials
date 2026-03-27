54178cbcaf7a7b4ccd17775c3e38ead0e4b5dbf6
nathalia-dssb <nathalia@vauxoo.com>
2026-03-27 16:09:37 -0600

    [IMP] estate: added access rules for property model in ir.model.access.csv
    Following the instructions of Chapter 4: Security - A Brief Introduction the security layer was added to the estate module.
    In the security/ir.model.access.csv file an ACL was defined that allows base users to interact with instances of the estate.property class, allowing creation, reading, writting and unlinking (deletion) of definitions of real estate properties definitions according to the model.
    
    Prior to this change no user was allowed to modify or create new properties in the estate module.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,14 +3,10 @@
     "summary": "Real estate advertising module for learning purposes, following the tutorial at Odoo documentation.",
     "author": "Vauxoo",
     "license": "LGPL-3",
-    "website": "https://www.yourcompany.com",
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    "category": "Tutorials",
+    "website": "https://www.vauxoo.com/",
+    "category": "Customizations",
     "version": "19.0.1.0.0",
-    # any module necessary for this one to work correctly
     "depends": ["base"],
-    # always loaded
+    "data": ['security/ir.model.access.csv',],
     "application": True,
 }
