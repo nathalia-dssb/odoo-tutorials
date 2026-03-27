6dc86d667b79cb0be158ba15bd6b23851d06ebca
nathalia-dssb <nathalia@vauxoo.com>
2026-03-27 11:36:42 -0600

    [ADD] estate: Initial module structure and definition
    [REF] estate: manifest adapted to the module implementation
    
    [REF] estate: adapted model definition with defined attributes
    
    [CLN] estate: linted code withouth white lines and file clean up
    
    [IMP] estate: adjustements to manifest and removed comments
    
    [IMP] estate: adjustements to manifest version number
    
    [IMP] estate: adjustements to manifest version number & generation of README.rst

---
+++ estate/README.rst
@@ -0,0 +1,11 @@
+===========
+Real Estate
+===========
+
+
+.. |badge1| image:: https://img.shields.io/badge/licence-LGPL--3-blue.png
+    :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+    :alt: License: LGPL-3
+
+|badge1|
+
---
+++ estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real Estate",
+    "summary": "Real estate advertising module for learning purposes, following the tutorial at Odoo documentation.",
+    "author": "Vauxoo",
+    "license": "LGPL-3",
+    "website": "https://www.yourcompany.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Tutorials",
+    "version": "19.0.1.0.0",
+    # any module necessary for this one to work correctly
+    "depends": ["base"],
+    # always loaded
+    "application": True,
+}
