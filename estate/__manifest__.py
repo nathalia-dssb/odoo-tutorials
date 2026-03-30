77efb487db50e93316ac6d2e0b3b63a896913921
nathalia-dssb <nathalia@vauxoo.com>
2026-03-30 10:31:18 -0600

    [IMP] estate: add estate property model view and root menu to the module
    On this addition the preliminary base definition of the view for the estate property model were made in
    estate_property_views.xml adding an action that was linked to the root menu of the module in estate_menus.xml
    so it becomes viewable and interactable to a base user in the Odoo interface.
    Additionally, the directories of the files mentioned earlier where added to the __manifest__.py so the views can be fetched by
    the module on installation or update.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,10 @@
     "category": "Customizations",
     "version": "19.0.1.0.0",
     "depends": ["base"],
-    "data": ['security/ir.model.access.csv',],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
     "application": True,
 }
