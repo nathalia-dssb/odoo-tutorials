02d9b4681531d67ef0df7946f98c31b391e05870
nathalia-dssb <nathalia@vauxoo.com>
2026-03-31 16:02:08 -0600

    [IMP] estate: add computed fields and onchange for property and offer
    According to the instructions of Chapter 8: Computed Fields And Onchanges from the Server Framework 101 Odoo tutorial, new attributes were added to estate_property.py to calculate the total area of a property by adding the garden area to the living area, and the best price is determined by the highest offer price of the property.
    An onchange method was also added to define default values when the garden checkbox is marked.
    
    On the estate.property.offer validity and date_deadline fields to estate.property.offer, where
    date_deadline is computed from create_date and validity, with an inverse
    to recalculate validity when deadline is set manually.
    
    Finally, fields for visualizing the validity, date deadline, best price and total area were added to the corresponding views of both the offer and property models.

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -15,3 +15,21 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = fields.Date.add(record.create_date, days=record.validity or 0)
+            else:
+                record.date_deadline = False
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date and record.date_deadline:
+                delta = record.date_deadline - record.create_date.date()
+                record.validity = delta.days
+            elif record.date_deadline:
+                record.validity = 7
