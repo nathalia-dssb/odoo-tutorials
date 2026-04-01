068906db3d4e66768b091acc304cd89402b09536
nathalia-dssb <nathalia@vauxoo.com>
2026-04-01 17:05:59 -0600

    [IMP] estate: improve styling and content disposition of module views
    Following chapter 11 (Sprinkles) of the Framework 101 tutorial, this adds styling improvements to the module. Starting with deterministic ordering to all of the modules and a manual sorting feature for the estate.property.types.
    A status bar widget was added to the estate.property form view to be able to view a state progression in a more notorious way.
    Inline list view of offers was added to estate.property.type to be able to view all the offers related to a certain estate property type.
    Conditional visibility were added to the garden related fields of estate.property, so if the garden checkbox is not selected then the garden_area and orientation wont appear.
    Readonly condition for estate.property form, so if the property is sold, with an accepted offer or cancelled, offers  won't be added.
    List decorations were added to show different states of a property and its offers.
    Now list view is editable for both estate.property.offer and estate.property.tag.
    The date_availability field of the estate.property is optional and will be hidden by default.
    By default 'available' filter will be selected when user enters the list view of estate.property.
    Lastly, a stat button was added to estate.property.type view to actively view offers.

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,10 +1,20 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate property type."
+    _order = "name"
 
     _unique_type_name = models.Constraint("unique (name)", "Property type name must be unique.")
 
     name = fields.Char(required=True, string="Type")
+    sequence = fields.Integer("Sequence", default=0, help="Used to order types alphabetically.")
+    property_ids = fields.One2many("estate.property", "property_type_id")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+    offer_count = fields.Integer(compute="_compute_offer_count", string="Number of offers")
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
