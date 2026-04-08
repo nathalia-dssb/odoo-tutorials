79f75dd90571d16004903a567eb8c82f06ea3e86
nathalia-dssb <nathalia@vauxoo.com>
2026-04-08 14:13:02 -0600

    [IMP] *: add generation of invoices on property sell
    The estate_account inherits from estate.property to extend the sold action, allowing invoice generation on property sell.
    Modifications were made on the estate module to comment the domain that only allows viewing of properties with new/offer_received state in the model view and for users, this is a requirement but for ease of development they're commented so it's easier to interact with all the properties on the module.

---
+++ estate_account/README.rst
@@ -0,0 +1,10 @@
+==================
+Real Estate Account
+==================
+
+
+.. |badge1| image:: https://img.shields.io/badge/licence-LGPL--3-blue.png
+    :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+    :alt: License: LGPL-3
+
+|badge1|
---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Real Estate Account",
+    "summary": "Real estate invoicing module.",
+    "author": "Vauxoo",
+    "license": "LGPL-3",
+    "website": "https://www.vauxoo.com/",
+    "category": "Customizations",
+    "version": "19.0.1.0.0",
+    "depends": ["estate", "account"],
+    "application": True,
+}
