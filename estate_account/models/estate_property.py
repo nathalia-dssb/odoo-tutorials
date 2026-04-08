4b92894a7f7bcd4aa3d46770fdbfd5eb958f31ac
nathalia-dssb <nathalia@vauxoo.com>
2026-04-08 14:13:02 -0600

    [IMP] *: add generation of invoices on property sell
    The estate_account inherits from estate.property to extend the sold action, allowing invoice generation on property sell.
    Modifications were made on the estate module to comment the domain that only allows viewing of properties with new/offer_received state in the model view and for users, this is a requirement but for ease of development they're commented so it's easier to interact with all the properties on the module.

---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,39 @@
+from odoo import models
+from odoo.fields import Command
+
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        super().action_sold()
+
+        journal = self.env["account.journal"].search(
+            [("type", "=", "sale")],
+            limit=1,
+        )
+
+        for prop in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": prop.buyer.id,
+                    "move_type": "out_invoice",
+                    "journal_id": journal.id,
+                    "invoice_line_ids": [
+                        Command.create(
+                            {
+                                "name": prop.name,
+                                "quantity": 1.0,
+                                "price_unit": prop.selling_price * 0.06,
+                            }
+                        ),
+                        Command.create(
+                            {
+                                "name": "Administrative fees",
+                                "quantity": 1.0,
+                                "price_unit": 100.0,
+                            }
+                        ),
+                    ],
+                }
+            )
